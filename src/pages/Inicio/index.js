49d959c26e5acf1adb2228e4a131a0de757973a7
Lucas Baptista <lucasbaptistasilva.dev@gmail.com>
2023-08-23 21:37:34 -0300

    Components CARD, TITULO e BANNER

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,11 +1,19 @@
+import Banner from "../../components/Banner";
 import Cabecalho from "../../components/Cabecalho";
+import Card from "../../components/Card";
 import Rodape from "../../components/Rodape";
+import Titulo from "../../components/Titulo";
 
 function Inicio(){
     return(
         <>
             <Cabecalho />
-            <h1>Olá rapaziada! teste</h1>
+            <Banner imagem="home"/>
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <Card id='1' titulo='MAAAAUZ' capa='https://thecatapi.com/api/images/get?format-src&type=png'/>
+            <Card id='2' titulo='OXEEEEN' capa='https://thecatapi.com/api/images/get?format-src&type=png'/>
             <Rodape />
         </>
     )
